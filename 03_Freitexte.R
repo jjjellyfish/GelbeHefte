9425074b8ec2bbf91b6f4736d3dae4d395911774
jjjellyfish <jgerb@protonmail.com>
2022-12-05 11:11:54 +0100

    Freitexte extrahiert

--- 03_Freitexte.R
+++ 03_Freitexte.R
@@ -4,7 +4,6 @@ library(xlsx)
 load('gh_clean.rda')
 
 # Notizen der Eltern
-# Freitexte rausziehen
 tmp <- dat %>% select(GH_ID, contains('ausgefüllt'), contains('Elterninfo_Freitext')) %>%
   pivot_longer(contains('Elterninfo_Freitext'), names_to='u', values_to = "var") %>%
   pivot_longer(contains('ausgefüllt'), names_to='u_data', values_to = "data") %>%
@@ -19,3 +18,20 @@ notizen <- tmp
 
 write.csv(notizen, '02_freitext_data/Freitexte_NotizenEltern.csv', row.names = FALSE)
 write.xlsx(notizen, file='02_freitext_data/Freitexte_NotizenEltern.xlsx')
+
+
+# Auffälligkeiten
+tmp <- dat %>% select(GH_ID, contains('ausgefüllt'), contains('Auffaelligkeiten_Freitext')) %>%
+  pivot_longer(contains('Auffaelligkeiten_Freitext'), names_to='u', values_to = "var") %>%
+  pivot_longer(contains('ausgefüllt'), names_to='u_data', values_to = "data") %>%
+  mutate(u=str_extract(u, "[^_]+"),
+         u_data=str_extract(u_data, "[^_]+")) %>%
+  filter(u==u_data & data==1) %>%
+  naniar::replace_with_na(replace = list(var = 99)) %>%
+  select(-u_data, -data) %>% 
+  na.omit()
+
+auff <- tmp
+
+write.csv(auff, '02_freitext_data/Freitexte_Auffaelligkeiten.csv', row.names = FALSE)
+write.xlsx(auff, file='02_freitext_data/Freitexte_Auffaelligkeiten.xlsx')
